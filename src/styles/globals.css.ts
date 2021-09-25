1e53f7a95fa95dd4ee23b5279c6fdd4560e13636
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-09-25 17:31:31 -0600

    chore(frontend): remove boilerplate code

--- src/styles/globals.css.ts
+++ src/styles/globals.css.ts
@@ -2,17 +2,6 @@ import { globalStyle } from '@vanilla-extract/css';
 
 import { vars } from '@/styles/vars.css';
 
-globalStyle('html, body', {
-  padding: 0,
-  margin: 0,
-  fontFamily: vars.fonts.main,
-});
-
-globalStyle('a', {
-  color: 'inherit',
-  textDecoration: 'none',
-});
-
 globalStyle('*', {
   boxSizing: 'border-box',
 });
--- src/styles/vars.css.ts
+++ src/styles/vars.css.ts
@@ -1,11 +1,3 @@
 import { createGlobalTheme } from '@vanilla-extract/css';
 
-export const vars = createGlobalTheme(':root', {
-  colors: {
-    main: '#0070f3',
-  },
-  fonts: {
-    main: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
-    code: 'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace',
-  },
-});
+export const vars = createGlobalTheme(':root', {});
